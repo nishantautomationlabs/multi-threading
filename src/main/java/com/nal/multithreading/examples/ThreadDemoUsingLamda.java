75c37f31ea6dc97afadfb3c4581b978d03fd4418
nishantautomationlabs <automationbynishant@gmail.com>
2019-12-22 20:09:24 +0530

    test change

--- src/main/java/com/nal/multithreading/examples/ThreadDemoUsingLamda.java
+++ src/main/java/com/nal/multithreading/examples/ThreadDemoUsingLamda.java
@@ -7,6 +7,7 @@ import com.nal.multithreading.Util.Utils;
  */
 public class ThreadDemoUsingLamda {
 
+    
     public static void main(String[] args) throws InterruptedException {
 
         Thread firstNameThread = new Thread(() -> {
